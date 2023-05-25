98b5f430d4e3928ea68225ea9ca3adc7cf377c69
AmirSolt <soltamir194@gmail.com>
2023-05-25 05:00:01 -0400

    small changes to userInput

--- src/lib/components/compare/ui/compareStages/CompareUserInput.svelte
+++ src/lib/components/compare/ui/compareStages/CompareUserInput.svelte
@@ -3,19 +3,29 @@
     export let isOver:boolean = false;
     export let userInput:{} = {};
 
-    userInput.input = ""
 
+    async function submitForm(e){
+        const form = e.target
+        let formData = new FormData(form)
+        // userInput. = formData.get('email')?.toString()?? ''
 
+        console.log(formData)
+    }
 </script>
 
 
-
-<label class="label">
-	<span>Textarea</span>
-	<textarea bind:value={userInput.input} class="textarea" rows="4" placeholder="Lorem ipsum dolor sit amet consectetur adipisicing elit." />
-</label>
-
-
-<button type="button" class="btn variant-filled" on:click={()=>isOver=true}>
-    Submit
-</button>
+<form on:submit|preventDefault={submitForm}>
+    <div>
+        <label for="email">Email</label>
+        <input class="w-full" type="email" name="email" id="email" autocomplete="username" required>
+        <br>
+        <label for="password">Password</label>
+        <input class="w-full" type="password" name="password" id="password" autocomplete="current-password" required>
+        <br>
+        <span>Textarea</span>
+        <textarea name="To avoid" class="textarea" rows="4" placeholder="Lorem ipsum dolor sit amet consectetur adipisicing elit." />
+    </div>
+    <br>
+    <button class="btn variant-filled w-full" type="submit">Submit</button>
+  
+</form>
